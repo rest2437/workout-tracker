a8a5f0a5733dafb4385c350bbb0922b564adec24
rest2437 <nocturnalrd@gmail.com>
2021-09-16 14:48:58 -0700

    changes: css updated

--- src/Bench.js
+++ src/Bench.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import * as VFX from "react-vfx";
 import "./Bench.css";
 
 function Bench(props) {
   const [state, setState] = useState({ buffPoints: 1 });
+
   function increaseBuff() {
     const newBuffPoints = state.buffPoints >= 10 ? 0 : state.buffPoints + 1;
     setState({ buffPoints: newBuffPoints });
@@ -16,8 +18,16 @@ function Bench(props) {
   return (
     <div>
       <div className="title">
+        <VFX.VFXProvider>
+          <VFX.VFXSpan shader="rgbShift">WHATS UP BUFFNESS?</VFX.VFXSpan>
+        </VFX.VFXProvider>
         <p>On a scale of 1-10 how did you do at the gym today? </p>
-        <p>BE HONEST... did you even go?</p>
+        <p>BE HONEST...</p>
+        <VFX.VFXProvider>
+          <VFX.VFXSpan class="go" shader="blink">
+            Did you even go?
+          </VFX.VFXSpan>
+        </VFX.VFXProvider>
       </div>
       <div className="bench">
         {" "}
